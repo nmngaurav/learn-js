e35c22916023bfbf3f012cf4ff6e2bcf03028040
nmngaurav <47002163+nmngaurav@users.noreply.github.com>
2025-06-19 04:25:56 +0000

    converstion

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,22 @@
+let score = null
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let test = true;
+let test = "33abc";
+let convertedValue = Number(test);
+// console.log(typeof test);
+
+// console.log(convertedValue);
+
+// 1=> true; 0=> false
+// "" => false
+// "asdfh" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
